58cd3a5e156597cdacc144725d11ab7c2efdf016
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-15 14:33:50 -0400

    add tsx in order to get app to run

--- src/app.ts
+++ src/app.ts
@@ -18,9 +18,9 @@ mongoose.connection.on('connected', () => {
 app.use(cors());
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.send('Hello, TypeScript Node Express!');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript Node Express!');
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
