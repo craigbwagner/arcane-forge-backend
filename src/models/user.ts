63764ef2a0aa7e7fcf330a46e0e100980e3231d5
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-15 15:04:22 -0400

    add character schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,40 @@
 import mongoose from "mongoose";
 
+const characterSchema = new mongoose.Schema({
+  name: String,
+  race: String,
+  classes: [{name: String, subclass: String, level: Number}],
+  level: {
+    type: Number,
+    max: 20,
+  },
+  sex: String,
+  size: String,
+  age: Number,
+  height: String,
+  weight: Number,
+  alignment: String,
+  languages: [String],
+  initiative: Number,
+  initiativeMods: Number,
+  speed: Number,
+  maxHP: Number,
+  currentHP: Number,
+  tempHP: Number,
+  hitDiceRemaining: Number,
+  hitDiceType: String,
+  hitDiceTotal: Number,
+  strength: Number,
+  dexterity: Number,
+  constitution: Number,
+  intelligence: Number,
+  wisdom: Number,
+  charisma: Number,
+  savingThrowProficiencies: [String],
+  skillProficiencies: [String],
+  skillExpertise: [String],
+}, {timestamps: true});
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -10,7 +45,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  characterSheets: [],
+  characterSheets: [characterSchema],
 });
 
 userSchema.set('toJSON', {
