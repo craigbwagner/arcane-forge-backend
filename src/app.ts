10c6c97fe8266069fe7b0cffe8a2acec6bb2741c
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-14 21:11:28 -0400

    add basic middleware and mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
-import express from 'express';
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript Node Express!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
