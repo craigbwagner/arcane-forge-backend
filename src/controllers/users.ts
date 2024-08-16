22a2ad2c58026607d7ff882ae7ca1fe6b74e9ddc
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-16 16:02:56 -0400

    add UserDocument type alias

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -5,18 +5,24 @@ import jwt from "jsonwebtoken";
 
 const SALT_LENGTH = 12;
 
+type UserDocument = {
+  username: string;
+  password: string;
+  _id: ObjectId;
+} | null;
+
 async function signUp(req:Request, res: Response) {
   try {
     const userInDatabase = await User.findOne({ username: req.body.username });
     if (userInDatabase) {
       return res.status(400).json({ error: "Username already taken." });
     }
-    const user = await User.create({
+    const user: UserDocument = await User.create({
       username: req.body.username,
       password: bcrypt.hashSync(req.body.password, SALT_LENGTH),
     });
     const token: string = generateToken(user);
-    res.status(201).json({ user, token });
+    res.status(201).json({ username: user.username, id: user._id, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -24,7 +30,7 @@ async function signUp(req:Request, res: Response) {
 
 async function signIn(req: Request, res: Response) {
   try {
-    const user = await User.findOne({ username: req.body.username });
+    const user: UserDocument = await User.findOne({ username: req.body.username });
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
       const token = generateToken(user);
       res.status(200).json({ token });
@@ -36,7 +42,7 @@ async function signIn(req: Request, res: Response) {
   }
 }
 
-function generateToken(user): string {
+function generateToken(user: UserDocument): string {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET environment variable is not defined.");
   }
