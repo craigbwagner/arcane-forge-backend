3cfb13fafa753a9deb30856c184e3fa71b75f230
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-15 23:54:17 -0400

    scaffold users controllers

---
+++ src/controllers/users.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+
+const SALT_LENGTH = 12;
+
+async function signUp(req:Request, res: Response) {
+  try {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+      return res.status(400).json({ error: "Username already taken." });
+    }
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, SALT_LENGTH),
+    });
+    const token = generateToken(user);
+    res.status(201).json({ user, token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+}
+
+async function signIn(req: Request, res: Response) {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ error: "Invalid username or password." });
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+}
+
+function generateToken(user: { username: string, _id: string }) {
+  const token = jwt.sign({ username: user.username, _id: user._id }, process.env.JWT_SECRET);
+  return token;
+}
+
+export default { signUp, signIn };
