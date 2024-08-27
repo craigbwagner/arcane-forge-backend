cc02ee08cf87cd0f67f00bf3fc6f202ff3140314
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-27 00:30:14 -0400

    add path for charactersRouter

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use("/users", usersRouter);
 
 app.use(verifyToken);
 
-app.use(charactersRouter);
+app.use("/characters", charactersRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
