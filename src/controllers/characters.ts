cfa86d66f28b69188f13c9ed8f09b4778539f719
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-28 12:30:58 -0400

    add destroy function

--- src/controllers/characters.ts
+++ src/controllers/characters.ts
@@ -47,4 +47,20 @@ async function update(req: Request, res: Response) {
   }
 }
 
-  export default { index, create, update };
+async function destroy(req: Request, res: Response) {
+  try {
+    const character: CharacterDocument | null = await Character.findById(req.params.id);
+    if (!character) {
+      return res.status(404).json({ error: "Character not found." });
+    } else {
+      await Character.findByIdAndDelete(req.params.id);
+      res.status(204).end();
+    }
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return res.status(400).json({ error: err.message });
+    }
+  }
+}
+
+  export default { index, create, update, destroy };
