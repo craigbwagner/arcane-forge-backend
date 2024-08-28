3e047607502f49c634de73cd24c557ab2bda6c80
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-08-28 12:31:40 -0400

    add delete route

--- src/routes/characters.ts
+++ src/routes/characters.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/', charactersController.index);
 router.post('/new', charactersController.create);
 router.put('/:id', charactersController.update);
+router.delete('/:id', charactersController.destroy);
 
 export default router;
