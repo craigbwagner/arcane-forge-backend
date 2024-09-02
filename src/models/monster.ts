d1ad5cdd66edb25ce9a2bfe9cd67b2d4d0ac4800
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-02 16:00:08 -0400

    add monster model and schema

---
+++ src/models/monster.ts
@@ -0,0 +1,26 @@
+import { Schema, Types, model } from "mongoose";
+
+const monsterSchema = new Schema({
+  name: String,
+  size: String,
+  type: String,
+  alignment: String,
+  armorClass: Number,
+  speed: [String],
+  strength: Number,
+  dexterity: Number,
+  constitution: Number,
+  intelligence: Number,
+  wisdom: Number,
+  charisma: Number,
+  maxHP: Number,
+  currentHP: Number,
+  tempHP: Number,
+  abilities: [String],
+  actions: [String],
+  bonusActions: [String]
+}, {timestamps: true});
+
+const Monster = model('Monster', monsterSchema);
+
+export { Monster };
