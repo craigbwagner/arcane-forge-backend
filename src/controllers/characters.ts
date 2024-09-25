8d710a81d53de89654d43ef17d68f431299a50b5
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-09-25 13:54:19 -0400

    remove console log

--- src/controllers/characters.ts
+++ src/controllers/characters.ts
@@ -74,7 +74,6 @@ async function destroy(req: Request, res: Response) {
       return res.status(404).json({ error: "Character not found." });
     } else {
       const user = await User.findById(character.creator)
-      console.log(character._id)
       if(!user) {
         return res.status(404).json({ error: "User not found." })
       }
